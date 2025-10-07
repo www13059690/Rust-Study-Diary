06f733b7ce9a233dc54a4655b81b69eeae58084e
Nero <774030473@qq.com>
2025-10-07 13:02:53 +0800

    update

---
+++ Project/module-test/restaurant/src/lib.rs
@@ -0,0 +1,16 @@
+// 餐厅前厅，用于吃饭
+mod front_of_house {
+    mod hosting {
+        fn add_to_waitlist() {}
+
+        fn seat_at_table() {}
+    }
+
+    mod serving {
+        fn take_order() {}
+
+        fn serve_order() {}
+
+        fn take_payment() {}
+    }
+}
